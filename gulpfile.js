f8af1664c1d1598b121535122d224d9a6ff51a02
Vladislav Korbut <v.korbut8@gmail.com>
2017-05-09 16:54:10 +0300

    files was replaced

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var browserSync = require('browser-sync').create();
  
 gulp.task('webserver', function() {
-  gulp.src('./app')
+  gulp.src('./')
     .pipe(webserver({
       livereload: true,
-      directoryListing: true,
+      directoryListing: false,
       open: true,
       fallback: './index.html'
     }));
+});
+
+
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("*.html").on("change", reload);
 });
